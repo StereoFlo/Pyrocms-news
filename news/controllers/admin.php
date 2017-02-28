5e529e353527296ee1f4c33dddccec52bd73fe13
StereoFlo <StereoFlo@users.noreply.github.com>
2017-02-28 22:06:29 +0300

    Syntax

--- news/controllers/admin.php
+++ news/controllers/admin.php
@@ -1,57 +1,74 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Class Admin
+ */
 class Admin extends Admin_Controller
 {
+    /**
+     * @var string
+     */
 	public $section = 'news';
+
+    /**
+     * @var int
+     */
 	protected $id = 0;
-	protected $validation_rules = array(
-		array(
+
+    /**
+     * @var array
+     */
+	protected $validation_rules = [
+		[
 			'field'	=> 'title',
 			'label'	=> 'lang:news_title_label',
 			'rules'	=> 'trim|htmlspecialchars|required|max_length[100]|callback__check_title'
-		),
-		array(
+        ],
+		[
 			'field'	=> 'slug',
 			'label'	=> 'lang:news_slug_label',
 			'rules' => 'trim|required|alpha_dot_dash|max_length[100]|callback__check_slug'
-		),
-		array(
+        ],
+		[
 			'field' => 'category_id',
 			'label' => 'lang:news_category_label',
 			'rules' => 'trim|numeric'
-		),
-		array(
+        ],
+		[
 			'field' => 'intro',
 			'label' => 'lang:news_intro_label',
 			'rules' => 'trim|required'
-		),
-		array(
+        ],
+		[
 			'field' => 'body',
 			'label' => 'lang:news_content_label',
 			'rules' => 'trim|required'
-		),
-		array(
+        ],
+		[
 			'field' => 'status',
 			'label' => 'lang:news_status_label',
 			'rules' => 'trim|alpha'
-		),
-		array(
+        ],
+		[
 			'field' => 'created_on',
 			'label' => 'lang:news_date_label',
 			'rules' => 'trim|required'
-		),
-		array(
+        ],
+		[
 			'field' => 'created_on_hour',
 			'label' => 'lang:news_created_hour',
 			'rules' => 'trim|numeric|required'
-		),
-		array(
+        ],
+		[
 			'field' => 'created_on_minute',
 			'label' => 'lang:news_created_minute',
 			'rules' => 'trim|numeric|required'
-		)
-	);
+        ]
+    ];
 
+    /**
+     * Admin constructor.
+     */
 	public function __construct()
 	{
 		parent::__construct();
@@ -63,30 +80,32 @@ class Admin extends Admin_Controller
 		
 		$this->data->hours = array_combine($hours = range(0, 23), $hours);
 		$this->data->minutes = array_combine($minutes = range(0, 59), $minutes);
-		
-		$this->data->categories = array(0 => '');
-		if ($categories = $this->news_categories_m->get_all())
-		{
-			foreach($categories as $category)
-			{
-				$this->data->categories[$category->id] = $category->title;
-			}
-		}
+
+        $this->data->categories = [0 => ''];
+        if ($categories = $this->news_categories_m->get_all()) {
+            foreach ($categories as $category) {
+                $this->data->categories[$category->id] = $category->title;
+            }
+        }
 		
 		$this->template
 			->append_metadata( css('news.css', 'news') )
 			->set_partial('shortcuts', 'admin/partials/shortcuts');
 	}
-	
+
+    /**
+     *
+     */
 	public function index()
 	{
-		$total_rows = $this->news_m->count_by(array('show_future'=>TRUE, 'status' => 'all'));
+		$total_rows = $this->news_m->count_by([
+		    'show_future' => true,
+            'status' => 'all']);
 		$pagination = create_pagination('admin/news/index', $total_rows);
-		
-		$news = $this->news_m->limit($pagination['limit'])->get_many_by(array(
+		$news = $this->news_m->limit($pagination['limit'])->get_many_by([
 			'show_future' => TRUE,
 			'status' => 'all'
-		));
+        ]);
 		
 		
 		$this->template
@@ -95,282 +114,257 @@ class Admin extends Admin_Controller
 			->set('news', $news)
 			->build('admin/index', $this->data);
 	}
-	
-	public function create()
-	{
-		$this->load->library('form_validation');
 
-		$this->form_validation->set_rules($this->validation_rules);
+    /**
+     *
+     */
+    public function create()
+    {
+        $article = new stdClass();
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules($this->validation_rules);
+        if ($this->input->post('created_on')) {
+            $created_on = strtotime(sprintf('%s %s:%s', $this->input->post('created_on'), $this->input->post('created_on_hour'), $this->input->post('created_on_minute')));
+        } else {
+            $created_on = now();
+        }
 
-		if ($this->input->post('created_on'))
-		{
-			$created_on = strtotime(sprintf('%s %s:%s', $this->input->post('created_on'), $this->input->post('created_on_hour'), $this->input->post('created_on_minute')));
-		}
+        if ($this->form_validation->run()) {
+            if ($this->input->post('category_id') == 0) {
+                $this->session->set_flashdata('error', $this->lang->line('news_article_add_error'));
+                redirect('admin/news/create');
+            }
 
-		else
-		{
-			$created_on = now();
-		}
+            $id = $this->news_m->insert(array(
+                'title' => $this->input->post('title'),
+                'slug' => $this->input->post('slug'),
+                'category_id' => $this->input->post('category_id'),
+                'intro' => $this->input->post('intro'),
+                'body' => $this->input->post('body'),
+                'status' => $this->input->post('status'),
+                'created_on' => $created_on
+            ));
 
-		if ($this->form_validation->run())
-		{
-			if ($this->input->post('category_id') == 0)
-			{
-				$this->session->set_flashdata('error', $this->lang->line('news_article_add_error'));
-				redirect('admin/news/create');
-			}
-			
-			$id = $this->news_m->insert(array(
-				'title'			=> $this->input->post('title'),
-				'slug'			=> $this->input->post('slug'),
-				'category_id'		=> $this->input->post('category_id'),
-				'intro'			=> $this->input->post('intro'),
-				'body'			=> $this->input->post('body'),
-				'status'		=> $this->input->post('status'),
-				'created_on' => $created_on
-			));
-			
-			if($id)
-			{
-				$this->session->set_flashdata('success', sprintf($this->lang->line('news_article_add_success'), $this->input->post('title')));
-			}
-			else
-			{
-				$this->session->set_flashdata('error', $this->lang->line('news_article_add_error'));
-			}
-			$this->input->post('btnAction') == 'save_exit' ? redirect('admin/news') : redirect('admin/news/edit/'.$id);
-		}
+            if ($id) {
+                $this->session->set_flashdata('success', sprintf($this->lang->line('news_article_add_success'), $this->input->post('title')));
+            } else {
+                $this->session->set_flashdata('error', $this->lang->line('news_article_add_error'));
+            }
+            $this->input->post('btnAction') == 'save_exit' ? redirect('admin/news') : redirect('admin/news/edit/' . $id);
+        } else {
+            foreach ($this->validation_rules as $key => $field) {
+                $article->$field['field'] = set_value($field['field']);
+            }
+            $article->created_on = $created_on;
+        }
 
-		else
-		{
-			foreach($this->validation_rules as $key => $field)
-			{
-				$article->$field['field'] = set_value($field['field']);
-			}
-			$article->created_on = $created_on;
-		}
-		
-		$this->template
-			->title($this->module_details['name'], lang('news_create_title'))
-			->append_metadata( $this->load->view('fragments/wysiwyg', $this->data, TRUE) )
-			->append_metadata( js('news_form.js', 'news') )
-			->set('article', $article)
-			->build('admin/form');
-	}
-	
-	public function edit($id = 0)
-	{
-		$id OR redirect('admin/news');
-		
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules($this->validation_rules);
-			
-		$article = $this->news_m->get($id);
+        $this->template
+            ->title($this->module_details['name'], lang('news_create_title'))
+            ->append_metadata($this->load->view('fragments/wysiwyg', $this->data, TRUE))
+            ->append_metadata(js('news_form.js', 'news'))
+            ->set('article', $article)
+            ->build('admin/form');
+    }
 
-		if ($this->input->post('created_on'))
-		{
-			$created_on = strtotime(sprintf('%s %s:%s', $this->input->post('created_on'), $this->input->post('created_on_hour'), $this->input->post('created_on_minute')));
-		}
+    /**
+     * @param int $id
+     */
+    public function edit($id = 0)
+    {
+        $id || redirect('admin/news');
 
-		else
-		{
-			$created_on = $article->created_on;
-		}
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules($this->validation_rules);
 
-		$this->id = $article->id;
-		
-		if ($this->input->post()) //$this->form_validation->run()
-		{
-			$result = $this->news_m->update($id, array(
-				'title'			=> $this->input->post('title'),
-				'slug'			=> $this->input->post('slug'),
-				'category_id'	=> $this->input->post('category_id'),
-				'intro'			=> $this->input->post('intro'),
-				'body'			=> $this->input->post('body'),
-				'status'		=> $this->input->post('status'),
-				'created_on' => $created_on
-			));
-			
-			if ($result)
-			{
-				$this->session->set_flashdata(array('success'=> sprintf($this->lang->line('news_edit_success'), $this->input->post('title'))));
-			}
-			
-			else
-			{
-				$this->session->set_flashdata(array('error'=> $this->lang->line('news_edit_error')));
-			}
-			
-			$this->input->post('btnAction') == 'save_exit'
-				? redirect('admin/news')
-				: redirect('admin/news/edit/'.$id);
-		}
-		
-		foreach(array_keys($this->validation_rules) as $field)
-		{
-			if (isset($_POST[$field]))
-			{
-				$article->$field = $this->form_validation->$field;
-			}
-		}
+        $article = $this->news_m->get($id);
 
-		$article->created_on = $created_on;
-		
-		$this->template
-			->title($this->module_details['name'], sprintf(lang('news_edit_title'), $article->title))
-			->append_metadata( $this->load->view('fragments/wysiwyg', $this->data, TRUE) )
-			->append_metadata( js('news_form.js', 'news') )
-			->set('article', $article)
-			->build('admin/form');
-	}	
-	
-	public function preview($id = 0)
-	{
-		$article = $this->news_m->get($id);
+        if ($this->input->post('created_on')) {
+            $created_on = strtotime(sprintf('%s %s:%s', $this->input->post('created_on'), $this->input->post('created_on_hour'), $this->input->post('created_on_minute')));
+        } else {
+            $created_on = $article->created_on;
+        }
 
-		$this->template
-			->set_layout('modal', 'admin')
-			->set('article', $article)
-			->build('admin/preview');
-	}
-	
-	public function action()
-	{
-		switch($this->input->post('btnAction'))
-		{
-			case 'publish':
-				$this->publish();
-			break;
-			case 'delete':
-				$this->delete();
-			break;
-			default:
-				redirect('admin/news');
-			break;
-		}
-	}
-	
+        $this->id = $article->id;
 
-	public function publish($id = 0)
-	{
-		$ids = ($id) ? array($id) : $this->input->post('action_to');
-		
-		if ( ! empty($ids))
-		{
-			$article_titles = array();
-			foreach ($ids as $id)
-			{
-				if ($article = $this->news_m->get($id) )
-				{
-					$this->news_m->publish($id);			
-					$article_titles[] = $article->title;
-				}
-			}
-		}
-	
-		if ( ! empty($article_titles))
-		{
-			if ( count($article_titles) == 1 )
-			{
-				$this->session->set_flashdata('success', sprintf($this->lang->line('news_publish_success'), $article_titles[0]));
-			}			
-			else
-			{
-				$this->session->set_flashdata('success', sprintf($this->lang->line('news_mass_publish_success'), implode('", "', $article_titles)));
-			}
-		}		
-		else
-		{
-			$this->session->set_flashdata('notice', $this->lang->line('news_publish_error'));
-		}
-		
-		redirect('admin/news');
-	}
-	public function delete($id = 0)
-	{
-		$ids = ($id) ? array($id) : $this->input->post('action_to');
-		
-		if ( ! empty($ids))
-		{
-			$article_titles = array();
-			foreach ($ids as $id)
-			{
-				if ($article = $this->news_m->get($id) )
-				{
-					$this->news_m->delete($id);
-					$article_titles[] = $article->title;
-				}
-			}
-		}
-		
-		if ( ! empty($article_titles))
-		{
-			if ( count($article_titles) == 1 )
-			{
-				$this->session->set_flashdata('success', sprintf($this->lang->line('news_delete_success'), $article_titles[0]));
-			}
-			else
-			{
-				$this->session->set_flashdata('success', sprintf($this->lang->line('news_mass_delete_success'), implode('", "', $article_titles)));
-			}
-		}		
-		else
-		{
-			$this->session->set_flashdata('notice', lang('news_delete_error'));
-		}
-		
-		redirect('admin/news');
-	}
-	
-	public function _check_title($title = '')
-	{
-		if ( ! $this->news_m->check_exists('title', $title, $this->id))
-		{
-			$this->form_validation->set_message('_check_title', sprintf(lang('news_already_exist_error'), lang('news_title_label')));
-			return FALSE;
-		}
-		
-		return TRUE;
-	}
-	
-	public function _check_slug($slug = '')
-	{
-		if ( ! $this->news_m->check_exists('slug', $slug, $this->id))
-		{
-			$this->form_validation->set_message('_check_slug', sprintf(lang('news_already_exist_error'), lang('news_slug_label')));
-			return FALSE;
-		}
-		
-		return TRUE;
-	}
-	
-	public function ajax_filter()
-	{
-		$category = $this->input->post('f_category');
-		$status = $this->input->post('f_status');
-		$keywords = $this->input->post('f_keywords');
-	
-		$post_data = array();
-	
-		if ($status == 'live' OR $status == 'draft')
-		{
-			$post_data['status'] = $status;
-		}
-	
-		if ($category != 0)
-		{
-			$post_data['category_id'] = $category;
-		}
-		
-		if ($keywords)
-		{
-			$post_data['keywords'] = $keywords;
-		}
-		$results = $this->news_m->search($post_data);
-	
-		$this->template
-			->set_layout(FALSE)
-			->set('news', $results)
-			->build('admin/index');
-	}
+        if ($this->input->post()) //$this->form_validation->run()
+        {
+            $result = $this->news_m->update($id, [
+                'title' => $this->input->post('title'),
+                'slug' => $this->input->post('slug'),
+                'category_id' => $this->input->post('category_id'),
+                'intro' => $this->input->post('intro'),
+                'body' => $this->input->post('body'),
+                'status' => $this->input->post('status'),
+                'created_on' => $created_on
+            ]);
+
+            if ($result) {
+                $this->session->set_flashdata(['success' => sprintf($this->lang->line('news_edit_success'), $this->input->post('title'))]);
+            } else {
+                $this->session->set_flashdata(['error' => $this->lang->line('news_edit_error')]);
+            }
+
+            $this->input->post('btnAction') == 'save_exit'
+                ? redirect('admin/news')
+                : redirect('admin/news/edit/' . $id);
+        }
+
+        foreach (array_keys($this->validation_rules) as $field) {
+            if (isset($_POST[$field])) {
+                $article->$field = $this->form_validation->$field;
+            }
+        }
+
+        $article->created_on = $created_on;
+
+        $this->template
+            ->title($this->module_details['name'], sprintf(lang('news_edit_title'), $article->title))
+            ->append_metadata($this->load->view('fragments/wysiwyg', $this->data, TRUE))
+            ->append_metadata(js('news_form.js', 'news'))
+            ->set('article', $article)
+            ->build('admin/form');
+    }
+
+    /**
+     * @param int $id
+     */
+    public function preview($id = 0)
+    {
+        $article = $this->news_m->get($id);
+
+        $this->template
+            ->set_layout('modal', 'admin')
+            ->set('article', $article)
+            ->build('admin/preview');
+    }
+
+    public function action()
+    {
+        switch ($this->input->post('btnAction')) {
+            case 'publish':
+                $this->publish();
+                break;
+            case 'delete':
+                $this->delete();
+                break;
+            default:
+                redirect('admin/news');
+                break;
+        }
+    }
+
+    /**
+     * @param int $id
+     */
+    public function publish($id = 0)
+    {
+        $ids = ($id) ? array($id) : $this->input->post('action_to');
+
+        if (!empty($ids)) {
+            $article_titles = array();
+            foreach ($ids as $id) {
+                if ($article = $this->news_m->get($id)) {
+                    $this->news_m->publish($id);
+                    $article_titles[] = $article->title;
+                }
+            }
+        }
+
+        if (!empty($article_titles)) {
+            if (count($article_titles) == 1) {
+                $this->session->set_flashdata('success', sprintf($this->lang->line('news_publish_success'), $article_titles[0]));
+            } else {
+                $this->session->set_flashdata('success', sprintf($this->lang->line('news_mass_publish_success'), implode('", "', $article_titles)));
+            }
+        } else {
+            $this->session->set_flashdata('notice', $this->lang->line('news_publish_error'));
+        }
+
+        redirect('admin/news');
+    }
+
+    public function delete($id = 0)
+    {
+        $ids = ($id) ? [$id] : $this->input->post('action_to');
+
+        if (!empty($ids)) {
+            $article_titles = array();
+            foreach ($ids as $id) {
+                if ($article = $this->news_m->get($id)) {
+                    $this->news_m->delete($id);
+                    $article_titles[] = $article->title;
+                }
+            }
+        }
+
+        if (!empty($article_titles)) {
+            if (count($article_titles) == 1) {
+                $this->session->set_flashdata('success', sprintf($this->lang->line('news_delete_success'), $article_titles[0]));
+            } else {
+                $this->session->set_flashdata('success', sprintf($this->lang->line('news_mass_delete_success'), implode('", "', $article_titles)));
+            }
+        } else {
+            $this->session->set_flashdata('notice', lang('news_delete_error'));
+        }
+
+        redirect('admin/news');
+    }
+
+    /**
+     * @param string $title
+     * @return bool
+     */
+    public function _check_title($title = '')
+    {
+        if (!$this->news_m->check_exists('title', $title, $this->id)) {
+            $this->form_validation->set_message('_check_title', sprintf(lang('news_already_exist_error'), lang('news_title_label')));
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $slug
+     * @return bool
+     */
+    public function _check_slug($slug = '')
+    {
+        if (!$this->news_m->check_exists('slug', $slug, $this->id)) {
+            $this->form_validation->set_message('_check_slug', sprintf(lang('news_already_exist_error'), lang('news_slug_label')));
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     */
+    public function ajax_filter()
+    {
+        $category = $this->input->post('f_category');
+        $status = $this->input->post('f_status');
+        $keywords = $this->input->post('f_keywords');
+
+        $post_data = array();
+
+        if ($status == 'live' OR $status == 'draft') {
+            $post_data['status'] = $status;
+        }
+
+        if ($category != 0) {
+            $post_data['category_id'] = $category;
+        }
+
+        if ($keywords) {
+            $post_data['keywords'] = $keywords;
+        }
+        $results = $this->news_m->search($post_data);
+
+        $this->template
+            ->set_layout(FALSE)
+            ->set('news', $results)
+            ->build('admin/index');
+    }
 }
--- news/details.php
+++ news/details.php
@@ -6,52 +6,52 @@ class Module_News extends Module {
 
 	public function info()
 	{
-		return array(
-			'name' => array(
+		return [
+			'name' => [
 				'en' => 'News',
 				'ru' => 'Новости',
-			),
-			'description' => array(
+            ],
+			'description' => [
 				'en' => 'Post news articles and blog entries.',
 				'ru' => 'Управление новостными статьями и записями блога.',
-			),
+            ],
 			'frontend' => TRUE,
 			'backend' => TRUE,
 			'menu' => 'content',
-			'sections' => array(
-				'news' => array(
+			'sections' => [
+				'news' => [
 				    'name' => 'news_articles_title',
 				    'uri' => 'admin/news',
-					'shortcuts'	=> array(
-						array(
+					'shortcuts'	=> [
+						[
 					 	   'name'	=> 'news_create_title',
 						   'uri'	=> 'admin/news/create',
 						   'class'	=> 'add'
-						)
-					)
-				),
-				'categories' => array(
+                        ]
+                    ]
+                ],
+				'categories' => [
 				    'name' => 'cat_list_title',
 				    'uri' => 'admin/news/categories',
-					'shortcuts'	=> array(
-						array(
+					'shortcuts'	=> [
+						[
 					 	   'name'	=> 'cat_create_title',
 						   'uri'	=> 'admin/news/categories/create',
 						   'class'	=> 'add'
-						)
-					)
-				),
-			),
-		);
+                        ]
+                    ]
+                ],
+            ],
+        ];
 	}
 
 	public function install()
 	{
-		$this->dbforge->drop_table('news_categories');
-		$this->dbforge->drop_table('news');
-		
-		$news_categories = "
-			CREATE TABLE ".$this->db->dbprefix('news_categories')." (
+        $this->dbforge->drop_table('news_categories');
+        $this->dbforge->drop_table('news');
+
+        $news_categories = "
+			CREATE TABLE " . $this->db->dbprefix('news_categories') . " (
 			  `id` int(11) NOT NULL auto_increment,
 			  `slug` varchar(20) collate utf8_unicode_ci NOT NULL default '',
 			  `title` varchar(20) collate utf8_unicode_ci NOT NULL default '',
@@ -62,8 +62,8 @@ class Module_News extends Module {
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='News Categories';
 		";
 
-		$news = "
-			CREATE TABLE ".$this->db->dbprefix('news')." (
+        $news = "
+			CREATE TABLE " . $this->db->dbprefix('news') . " (
 			  `id` int(11) NOT NULL auto_increment,
 			  `title` varchar(100) collate utf8_unicode_ci NOT NULL default '',
 			  `slug` varchar(100) collate utf8_unicode_ci NOT NULL default '',
@@ -80,30 +80,28 @@ class Module_News extends Module {
 			  KEY `category_id - normal` (`category_id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='News articles or blog posts.';
 		";
-		
-		if($this->db->query($news_categories) && $this->db->query($news))
-		{
-			return TRUE;
-		}
+
+        if (!$this->db->query($news_categories) || !$this->db->query($news)) {
+            return false;
+        }
+        return true;
 	}
 
-	public function uninstall()
-	{		
-		if($this->dbforge->drop_table('news_categories') &&
-		   $this->dbforge->drop_table('news'))
-		{
-			return TRUE;
-		}
-	}
+    public function uninstall()
+    {
+        if (!$this->dbforge->drop_table('news_categories') || !$this->dbforge->drop_table('news')) {
+            return false;
+        }
+        return true;
+    }
 
-	public function upgrade($old_version)
-	{
-		return TRUE;
-	}
+    public function upgrade($old_version)
+    {
+        return TRUE;
+    }
 
-	public function help()
-	{
-		return "<h4>Обзор</h4>
-				<p>Модуль новостей, чо</p>";
-	}
+    public function help()
+    {
+        return "<h4>Обзор</h4><p>Модуль новостей, чо</p>";
+    }
 }
